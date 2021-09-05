85e91677fec9189a66a89b14cac1a2b9ae7e7c0e
cameronreidalbin <cameron.tex.albin@gmail.com>
2021-09-05 13:50:43 -0500

    v16

--- HeadToHeadAustin/settings.py
+++ HeadToHeadAustin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6@4087t-9e9d3$x@&j112y!wsiq6-@g)d9wrm0ltfl%g6%+%4e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['stark-meadow-18039.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['head-to-head-austin.herokuapp.com','127.0.0.1']
 
 
 # Application definition
