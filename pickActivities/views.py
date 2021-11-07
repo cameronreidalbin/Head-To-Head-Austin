a1ba4a03cd53109a87870ae820f8366ba122d9f1
cameronreidalbin <cameron.tex.albin@gmail.com>
2021-11-07 12:49:00 -0600

    vacations v2

--- pickActivities/views.py
+++ pickActivities/views.py
@@ -18,9 +18,14 @@ def matchup(request):
     [option1RowNum,option2RowNum] = random.sample(optionNums,2)
     option1Title = sheet.get(1,option1RowNum)
     option2Title = sheet.get(1,option2RowNum)
-    pic1 = Image.open('pickActivities/static/pickActivities/' + option1Title + '.PNG')
-    pic2 = Image.open('pickActivities/static/pickActivities/' + option2Title + '.PNG')
-   
+    try:
+        pic1 = Image.open('pickActivities/static/pickActivities/' + option1Title + '.PNG')
+    except:
+        pic1 = Image.open('pickActivities/static/pickActivities/' + option1Title + '.png')
+    try:
+        pic2 = Image.open('pickActivities/static/pickActivities/' + option2Title + '.PNG')
+    except:
+        pic2 = Image.open('pickActivities/static/pickActivities/' + option2Title + '.png')
     pic1.save('pickActivities/static/pickActivities/pic1.PNG')
     pic2.save('pickActivities/static/pickActivities/pic2.PNG')
     context = {'option1Title':option1Title, 'option2Title':option2Title, 'option1RowNum':option1RowNum, 'option2RowNum':option2RowNum}
